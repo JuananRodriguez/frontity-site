a9430fcfa4f5fa631ad1c301d6611efae2955096
JuananRodriguez <juananrodriguezmartinez@gmail.com>
2020-05-30 16:44:59 +0200

    testing vercel integration

--- packages/mars-theme/src/components/header.js
+++ packages/mars-theme/src/components/header.js
@@ -10,6 +10,7 @@ const Header = ({ state }) => {
       <Container>
         <StyledLink link="/">
           <Title>{state.frontity.title}</Title>
+          <p>Probando CI con vercel</p>
         </StyledLink>
         <Description>{state.frontity.description}</Description>
         <MobileMenu />
